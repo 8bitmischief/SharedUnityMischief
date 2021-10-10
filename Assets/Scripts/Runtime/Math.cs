33df34830388816b6129409dfd64469ef23d8771
bridgs <bridgs.dev@gmail.com>
2021-10-10 11:15:38 -0400

    Adding Math.Remap

---
+++ Assets/Scripts/Runtime/Math.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace SharedUnityMischief {
+	public static class Math {
+        public static float Map (float value, float fromMin, float fromMax, bool clamp = false) => Map(value, fromMin, fromMax, 0f, 1f, clamp);
+
+		public static float Map (float value, float fromMin, float fromMax, float toMin, float toMax, bool clamp = false) {
+            float mappedValue = ((value - fromMin) / (fromMax - fromMin)) * (toMax - toMin) + toMin;
+            if (clamp)
+                return Mathf.Clamp(mappedValue, Mathf.Min(toMin, toMax), Mathf.Max(toMin, toMax));
+            else
+                return mappedValue;
+        }
+	}
+}
