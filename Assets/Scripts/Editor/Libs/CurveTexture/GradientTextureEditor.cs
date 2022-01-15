175e3c2b5190fe53f7be7a09d9c3b09493a32630
bridgs <bridgs.dev@gmail.com>
2022-01-14 23:10:28 -0500

    GradientTexture class to make makin' gradients a whole lot easier

---
+++ Assets/Scripts/Editor/Libs/CurveTexture/GradientTextureEditor.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace SharedUnityMischief
+{
+	[CustomEditor(typeof(GradientTexture))]
+	public class GradientTextureEditor : BaseEditor 
+	{
+		protected override void DrawControls()
+		{
+			GradientTexture gradientTexture = target as GradientTexture;
+			if (GUILayout.Button("Generate Texture"))
+			{
+				string path = AssetDatabase.GetAssetPath(gradientTexture);
+				path = Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path) + ".png");
+				bool didAssetAlreadyExist = !string.IsNullOrEmpty(AssetDatabase.AssetPathToGUID(path));
+				// Create the texture and save it to a PNG
+				Texture2D texture = gradientTexture.GenerateTexture();
+				byte[] bytes = texture.EncodeToPNG();
+				DestroyImmediate(texture);
+				File.WriteAllBytes(path, bytes);
+				// Import the new image asset
+				AssetDatabase.ImportAsset(path);
+				if (!didAssetAlreadyExist)
+				{
+					TextureImporter textureImporter = AssetImporter.GetAtPath(path) as TextureImporter;
+					textureImporter.sRGBTexture = true;
+					textureImporter.alphaIsTransparency = true;
+					textureImporter.wrapMode = TextureWrapMode.Clamp;
+					textureImporter.filterMode = FilterMode.Point;
+					textureImporter.mipmapEnabled = false;
+					textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+					textureImporter.SaveAndReimport();
+				}
+			}
+		}
+
+		protected override void DrawEditModeControls() => DrawControls();
+	}
+}
---
+++ Assets/Scripts/Runtime/Libs/CurveTexture/GradientTexture.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace SharedUnityMischief
+{
+	// This class is a modified version of the one from https://github.com/slipster216/CurveTexture
+	// LICENSE included in directory, thank you for the code!
+	[CreateAssetMenu(fileName = "New Gradient", menuName = "SharedUnityMischief/Gradient")]
+	public class GradientTexture : ScriptableObject 
+	{
+		[SerializeField] private Gradient _gradient;
+
+		public Texture2D GenerateTexture()
+		{
+			Texture2D texture = new Texture2D(128, 1, TextureFormat.ARGB32, false, true);
+			Color[] pixels = new Color[128];
+			for (int i = 0; i < pixels.Length; ++i)
+			{
+				float t = ((float) i) / ((float) (pixels.Length - 1));
+				pixels[i] = _gradient.Evaluate(t);
+			}
+			texture.SetPixels(pixels);
+			texture.Apply(false, false);
+			return texture;
+		}
+	}
+}
