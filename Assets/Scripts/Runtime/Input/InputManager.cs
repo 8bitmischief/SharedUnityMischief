70d1a487b49f3ddc681198e3d75267147c7222db
Ayla Myers <bridgs.dev@gmail.com>
2022-01-26 19:14:26 -0500

    Option for InputManager not to update InputSystem

--- Assets/Scripts/Runtime/Input/InputManager.cs
+++ Assets/Scripts/Runtime/Input/InputManager.cs
@@ -6,9 +6,13 @@ namespace SharedUnityMischief.Input
 	[DefaultExecutionOrder(-80)]
 	public class InputManager : MonoBehaviour
 	{
+		[Header("Input System")]
+		[SerializeField] private bool updateInputSystem = false;
+
 		protected virtual void Update()
 		{
-			InputSystem.Update();
+			if (updateInputSystem)
+				InputSystem.Update();
 		}
 	}
 }
